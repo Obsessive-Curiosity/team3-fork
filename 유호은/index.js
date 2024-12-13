05a1dc0d4d8c8c720961f04339c929bbebe0aa6f
HoeunRyu <hoeun9565@gmail.com>
2024-12-13 16:30:29 +0900

    소개 버튼 수정, 댓글 추가

--- 유호은/index.js
+++ 유호은/index.js
@@ -1,6 +1,4 @@
 const myName = document.querySelector("#name");
-const aboutMeBtn = document.querySelector("#infoBtn");
-const cardMid = document.querySelectorAll(".cardMid");
 
 // 이름 클릭하면 랜덤 색상으로 바꾸기
 const getRandomNum = function () {
@@ -17,15 +15,37 @@ function colorChange(e) {
 myName.addEventListener("click", colorChange);
 
 //aboutMe 버튼 클릭하면 하단 정보 보이게 하기기
-const showAboutMeBtn = function () {
-    for (const item of cardMid) {
-        item.classList.toggle("show");
-    }
-}
+const aboutMeBtn = document.querySelector("#infoBtn");
+const cardHidden = document.querySelector("#hiddenCard");
 
-// for (const item of cardMid) {
-//     item.classList.add = "show";
-//     console.log(item);
-// }
+const showAboutMeBtn = function () {
+    cardHidden.classList.toggle("hidden");
+    // cardHidden.classList.toggle("show");
+}
+console.log(cardHidden)
 
 aboutMeBtn.addEventListener("click", showAboutMeBtn);
+
+//댓글 추가하기
+const commentInput = document.querySelector("#commentInput");
+const commentBtn = document.querySelector("#commentBtn");
+const commentContainer = document.querySelector("#commentContainer")
+
+const inputValue = function () {
+    const value = commentInput.value;
+
+    if (value === "") {
+        alert("댓글을 작성해주세요!");
+        return;
+    }
+
+    const newComment = document.createElement("div");
+    newComment.classList.add("comment");
+    newComment.textContent = value;
+
+    commentContainer.append(newComment);
+
+    commentInput.value = "";
+}
+
+commentBtn.addEventListener("click", inputValue);
