597d0885619c91d158cdcc27dc79eb278f87616d
hyuk-dev <hyuk.development@gmail.com>
2024-12-17 11:07:34 +0900

    1번부터 5번까지 문제입니다.

---
+++ 이동혁 자바스크립트 문제/5번.js
@@ -0,0 +1,31 @@
+/*
+    문제 의도: 함수 이해
+    
+    은행 업무 시스템을 만드려고 합니다.
+    기능에는 입금, 출금, 입출금내역 기록이 있습니다.
+
+    각 기능을 함수로 만들어서 입금, 출금을 한번 씩 하고
+    입출금내역을 출력해주세요.
+
+    조건: deposit(), withdrawal() 의 파라미터에 details() 를 콜백함수로 받아주세요.
+*/
+
+
+let money = 10000000;
+const detailsLog = [];
+
+function deposit () {
+
+}
+
+function withdrawal () {
+
+}
+
+function details () {
+
+}
+
+console.log(detailsLog);
+console.log();
+console.log('잔액: ' + money + '원');
