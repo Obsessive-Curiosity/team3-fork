0a2cdba53d450be21f8f36c57884d2eef311d43a
민지영 <kuck523@gmail.com>
2024-12-17 15:23:48 +0900

    fix: 변수명 헝가리안 표기법으로 변경

--- 가승연소개/gsy.js
+++ 가승연소개/gsy.js
@@ -1,61 +1,60 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const profileImg = document.querySelector(".profile-image");
-    const profileInfo = document.querySelector(".profile-info");
-    const bestPlaylist = document.querySelector(".best-playlist"); 
-    const playlist = document.querySelector(".playlist"); 
-    const bestmovie = document.querySelector(".best-movie"); 
-    const movie = document.querySelector(".movie"); 
-    const bestmenu = document.querySelector(".best-menu"); 
-    const menu = document.querySelector(".menu"); 
-    const besthobby = document.querySelector(".best-hobby"); 
-    const hobby = document.querySelector(".hobby"); 
-    const mytmi = document.querySelector(".my-tmi"); 
-    const tmi = document.querySelector(".tmi"); 
+document.addEventListener("DOMContentLoaded", function () {
+  const profileImg = document.querySelector(".profile-image");
+  const profileInfo = document.querySelector(".profile-info");
+  const bestPlaylist = document.querySelector(".best-playlist");
+  const playList = document.querySelector(".playlist");
+  const bestMovie = document.querySelector(".best-movie");
+  const movie = document.querySelector(".movie");
+  const bestMenu = document.querySelector(".best-menu");
+  const menu = document.querySelector(".menu");
+  const bestHobby = document.querySelector(".best-hobby");
+  const hobby = document.querySelector(".hobby");
+  const myTmi = document.querySelector(".my-tmi");
+  const tmi = document.querySelector(".tmi");
 
-    profileImg.addEventListener("mouseover", function() {
-        profileInfo.style.display = "block"; 
-    });
+  profileImg.addEventListener("mouseover", function () {
+    profileInfo.style.display = "block";
+  });
 
-    profileImg.addEventListener("mouseout", function() {
-        profileInfo.style.display = "none";
-    });
+  profileImg.addEventListener("mouseout", function () {
+    profileInfo.style.display = "none";
+  });
 
-    bestPlaylist.addEventListener("mouseenter", function() {
-        playlist.style.display = "block"; 
-      });
-    
-    bestPlaylist.addEventListener("mouseleave", function() {
-        playlist.style.display = "none"; 
-      });
+  bestPlaylist.addEventListener("mouseenter", function () {
+    playlist.style.display = "block";
+  });
 
-    bestmovie.addEventListener("mouseover", function() {
-        movie.style.display = "block"; 
-    });
+  bestPlaylist.addEventListener("mouseleave", function () {
+    playlist.style.display = "none";
+  });
 
-    bestmovie.addEventListener("mouseout", function() {
-        movie.style.display = "none"; 
-    });
-    
-    bestmenu.addEventListener("mouseover", function() {
-        menu.style.display = "block"; 
-    });
+  bestmovie.addEventListener("mouseover", function () {
+    movie.style.display = "block";
+  });
 
-    bestmenu.addEventListener("mouseout", function() {
-        menu.style.display = "none"; 
-    });
-    besthobby.addEventListener("mouseover", function() {
-        hobby.style.display = "block"; 
-    });
+  bestmovie.addEventListener("mouseout", function () {
+    movie.style.display = "none";
+  });
 
-    besthobby.addEventListener("mouseout", function() {
-        hobby.style.display = "none"; 
-    });
-    mytmi.addEventListener("mouseover", function() {
-        tmi.style.display = "block"; 
-    });
+  bestmenu.addEventListener("mouseover", function () {
+    menu.style.display = "block";
+  });
 
-    mytmi.addEventListener("mouseout", function() {
-        tmi.style.display = "none"; 
-    });
+  bestmenu.addEventListener("mouseout", function () {
+    menu.style.display = "none";
+  });
+  besthobby.addEventListener("mouseover", function () {
+    hobby.style.display = "block";
+  });
+
+  besthobby.addEventListener("mouseout", function () {
+    hobby.style.display = "none";
+  });
+  mytmi.addEventListener("mouseover", function () {
+    tmi.style.display = "block";
+  });
+
+  mytmi.addEventListener("mouseout", function () {
+    tmi.style.display = "none";
+  });
 });
-
